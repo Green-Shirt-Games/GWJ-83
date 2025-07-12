d8ef6bccf49c311bb8efd17c53e429c30e31f4b1
Jacob Dyer <33190456+JacobLD@users.noreply.github.com>
2025-07-12 14:50:31 -0500

    Basic master scene to handle moving between our main game scenes

---
+++ master_scene/door.gd
@@ -0,0 +1,18 @@
+extends Node2D
+class_name Door
+
+signal to_table_pressed
+
+@onready var to_table_button1 : Button = $Button
+@onready var to_table_button2 : Button = $Button2
+
+func _ready() -> void:
+	to_table_button1.pressed.connect(
+		func():
+			to_table_pressed.emit()
+	)
+	
+	to_table_button2.pressed.connect(
+		func():
+			to_table_pressed.emit()
+	)
