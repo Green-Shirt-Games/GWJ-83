3444b762f0796d727dafacbae6262ada4ee71523
Jacob Dyer <33190456+JacobLD@users.noreply.github.com>
2025-07-17 11:30:53 -0500

    Added new bar sprites and modified register to work on a per value basis

--- Scripts/BarScene/register.gd
+++ Scripts/BarScene/register.gd
@@ -5,17 +5,23 @@ signal buy_pressed(bottles : Array[Bottle], total_price : int)
 signal too_poor()
 
 @onready var buy_button : Button = $Button
-@onready var price_label : Label = $ColorRect/Label
 @onready var buy_area : Area2D = $BuyArea/Area2D
+@onready var values_parent : Node2D = $Values
 @export var bottle_parent : Bottle
 
 var running_total : int = 0
+var current_total_str : String = "000000000"
 var bottles : Array[Bottle] = []
+
+var value_labels : Array[Label] = []
 
 func _ready() -> void:
 	buy_button.pressed.connect(_on_buy_pressed)
 	buy_area.body_entered.connect(_on_body_enter)
 	buy_area.body_exited.connect(_on_body_exit)
+	
+	for child in values_parent.get_children():
+		value_labels.append(child)
 
 
 func _on_buy_pressed():
@@ -50,9 +56,19 @@ func _on_body_exit(body):
 
 
 func _update_price_ui():
-	var padded_str := "%07d" % running_total
-	padded_str = "$ " + padded_str
-	price_label.text = padded_str
+	var padded_str := "%09d" % running_total
+	
+	var index : int = 0
+	for i in range(padded_str.length()):
+		if padded_str[i] != current_total_str[i]:
+			_update_value_index(i, padded_str[i])
+	
+	current_total_str = padded_str
+
+
+func _update_value_index(index : int, new_value : String):
+	var label : Label = value_labels[index]
+	label.text = new_value
 
 
 func clear():
