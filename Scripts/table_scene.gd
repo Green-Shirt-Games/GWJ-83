ed10fc19801880969785a9a2004fbbd86de63dea
InVeRnyako <inver.nyako@gmail.com>
2025-07-20 21:54:47 +0300

    split hand buttons visibility fix

--- Scripts/table_scene.gd
+++ Scripts/table_scene.gd
@@ -411,6 +411,7 @@ func _on_split_button_pressed() -> void:
 		active_player_hand = 0
 		await _reveal_players_hand()
 		play_buttons_container.visible = true
+		_update_play_buttons_visibility()
 	else:
 		Global.not_enough_money.emit()
 
@@ -418,6 +419,7 @@ func _on_stand_button_pressed() -> void:
 	player_hands[active_player_hand].waiting_for_first_action = false
 	if active_player_hand == 0 and player_hands[1].is_active:
 		active_player_hand = 1
+		_update_play_buttons_visibility()
 	else:
 		_change_state(Global.GAME_STATES.DEALER_TURN)
 
@@ -430,11 +432,13 @@ func _on_double_down_button_pressed() -> void:
 		if player_hands[active_player_hand].bust:
 			if active_player_hand == 0 and player_hands[1].is_active:
 				active_player_hand = 1
+				_update_play_buttons_visibility()
 			else:
 				_change_state(Global.GAME_STATES.RESULT)
 		else:
 			if active_player_hand == 0 and player_hands[1].is_active:
 				active_player_hand = 1
+				_update_play_buttons_visibility()
 			else:
 				_change_state(Global.GAME_STATES.DEALER_TURN)
 	else:
