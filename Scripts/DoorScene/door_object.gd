83d2a078e2f006e05b84df5733e0d9a10d36deca
Jacob Dyer <33190456+JacobLD@users.noreply.github.com>
2025-07-20 12:22:54 -0500

    Update door_object.gd

--- Scripts/DoorScene/door_object.gd
+++ Scripts/DoorScene/door_object.gd
@@ -5,6 +5,7 @@ extends Sprite2D
 @onready var locked_audio : AudioStreamPlayer2D = $AudioStreamPlayer2D
 @export var unlockedBackground : Texture2D
 @export var backgroundSprite : Sprite2D
+@onready var lockedBackground : Texture2D = backgroundSprite.texture
 var locked : bool = true
 var door_open_count : int = 0
 
@@ -13,6 +14,7 @@ func _ready() -> void:
 	door_area.mouse_exited.connect(_on_mouse_exit)
 	door_area.input_event.connect(_input_event)
 	Global.key_purchased.connect(_unlock)
+	Global.final_hand_over.connect(_on_final_hand_over)
 
 func _input_event(_viewport, event, _shape_idx):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
@@ -23,14 +25,21 @@ func _on_select():
 	if locked:
 		locked_audio.play()
 	else:
+		locked_audio.play()
+		locked = true
+		backgroundSprite.texture = lockedBackground
 		door_open_count += 1
 		Global.player_exited_door.emit(door_open_count)
 
 
 func _unlock():
+	locked_audio.play()
 	backgroundSprite.texture = unlockedBackground
 	locked = false
 
+func _on_final_hand_over(won):
+	if !won: return
+	_unlock()
 
 func _on_mouse_enter():
 	pass
