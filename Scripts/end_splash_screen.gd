f208c6a3b9608b3683399d64c909b5bc2762dccd
InVeRnyako <inver.nyako@gmail.com>
2025-07-20 21:30:32 +0300

    Update end_splash_screen.gd

--- Scripts/end_splash_screen.gd
+++ Scripts/end_splash_screen.gd
@@ -6,7 +6,6 @@ extends Control
 func _ready() -> void:
 	if OS.has_feature("web"):
 		exit_button.visible = false
-	update_win_lose_label() # Pass bool that player won / lost
 
 
 func update_win_lose_label(player_won : bool = false) -> void:
